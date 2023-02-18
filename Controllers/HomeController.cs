2fe183fed29f89f5f2815e259d27b9fd57bcbb0d
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2023-02-18 14:11:14 +0300

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcValidation.Models;
+
+namespace MvcValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.HataVarMi = false;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Musteri musteri)
+        {
+            //kontrol et
+
+            //old way - manuel kontrol
+            //if(string.IsNullOrWhiteSpace(musteri.Adi) || musteri.SiparisSayisi == 0 || string.IsNullOrWhiteSpace(musteri.Email) || EmailDogruMu(musteri.Email) == false || musteri.KayitTarihi > DateTime.Now.AddDays(1))
+            //{
+            //    //hata ver
+            //    ViewBag.HataVarMi = true;
+            //}
+            //else
+            //{
+            //    //dbye kaydet
+            //}
+
+            if (ModelState.IsValid)
+            {
+                //dbye kaydet
+            }
+            else
+                ViewBag.HataVarMi = true;
+
+            return View();
+        }
+
+        private bool EmailDogruMu(string email)
+        {
+            if (email.Contains("@") == true && email.Contains("."))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Models/Musteri.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcValidation.Models
+{
+    public class Musteri
+    {
+        [Required(ErrorMessage = "Adı alanı zorunludur ve max 25 karakter olmalı")]
+        [StringLength(25)]
+        public string Adi { get; set; }
+                
+        public string? Adresi { get; set; }
+        public string Ulke { get; set; }
+
+        [Required(ErrorMessage = "Email alanı zorunludur ve geçerli bir adres olmalıdır")]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        public DateTime? KayitTarihi { get; set; }
+        public int? SiparisSayisi { get; set; }
+    }
+}
